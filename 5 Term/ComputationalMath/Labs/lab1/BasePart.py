4cba6aec28ab349c29830da74c86732074db426d
Stanislav_Notebook <stas280501@yandex.ru>
2021-09-15 11:19:35 +0300

    ComputationalMath lab1 BasePart done

--- 5 Term/ComputationalMath/Labs/lab1/BasePart.py
+++ 5 Term/ComputationalMath/Labs/lab1/BasePart.py
@@ -4,15 +4,18 @@ import numpy as np
 
 
 def сubic_spline_coeff(x_nodes, y_nodes):
+    a = y_nodes  # coef a
+
     h = x_nodes[1:len(x_nodes)] - x_nodes[0:len(x_nodes) - 1]  # store from h_1 to h_n-1
 
     up_diag = np.concatenate(([0], h[1:]))
     down_diag = np.concatenate((h[:-1], [0]))
     general_diag = np.concatenate(([1], (h[1:] + h[0:-1]) * 2, [1]))
 
-    a = y_nodes  # coef a
 
     matrix_A = np.diag(down_diag, -1) + np.diag(general_diag) + np.diag(up_diag, 1)
+
+
     matrix_B = np.concatenate(([0], 3 * np.subtract(np.divide(np.subtract(a[2:], a[1:-1]), h[1:]),
                                                     np.divide(np.subtract(a[1:-1], a[:-2]), h[:-1])), [0]))
 
@@ -27,35 +30,35 @@ def сubic_spline_coeff(x_nodes, y_nodes):
     return result_matrix
 
 
-def сubic_spline(x, qubicSplineCoeff):
-    index = list.index(x)
-    a = qubicSplineCoeff[index][0]
-    b = qubicSplineCoeff[index][1]
-    c = qubicSplineCoeff[index][2]
-    d = qubicSplineCoeff[index][3]
-    xi = qubicSplineCoeff[index][4]
+def сubic_spline(x, cubicSplineCoeff,x_nodes):
+
+    index = index_for_cubicSpline(x, x_nodes)
+    a = cubicSplineCoeff[index][0]
+    b = cubicSplineCoeff[index][1]
+    c = cubicSplineCoeff[index][2]
+    d = cubicSplineCoeff[index][3]
+    xi = cubicSplineCoeff[index][4]
 
     return a + b * (x - xi) + c * ((x - xi) ** 2) + d * ((x - xi) ** 3)
 
 
-def d_сubic_spline(x, qs_coeff):
-    index = list.index(x)
-    a = qubicSplineCoeff[index][0]
-    b = qubicSplineCoeff[index][1]
-    c = qubicSplineCoeff[index][2]
-    d = qubicSplineCoeff[index][3]
-    xi = qubicSplineCoeff[index][4]
+def d_сubic_spline(x, cubicSplineCoeff):
+    index = index_for_cubicSpline(x, cubicSplineCoeff[4])
+    a = cubicSplineCoeff[index][0]
+    b = cubicSplineCoeff[index][1]
+    c = cubicSplineCoeff[index][2]
+    d = cubicSplineCoeff[index][3]
+    xi = cubicSplineCoeff[index][4]
 
-    return b + 2 * c * (x - xi) + 3 * d * (x - xi)**2
+    return b + 2 * c * (x - xi) + 3 * d * (x - xi) ** 2
 
-def index_for_qubicSpline(x):
+
+def index_for_cubicSpline(x, x_nodes):
     n = len(x_nodes)
-    for i in range(n - 1):
-        if (x >= x_nodes[i] and x <= x_nodes[i + 1]):
-            index = i
-            break
-    if (x > x_nodes[n - 1]):
-        index = n - 2
-    if (x < x_nodes[0]):
-        index = 0
-    return index
+    for i in range(len(x_nodes) - 1):
+        if x_nodes[i] <= x <= x_nodes[i + 1]:
+            return i
+    if x_nodes[len(x_nodes) - 1] < x:
+        return len(x_nodes) - 2
+    if x_nodes[0] > x:
+        return 0
--- 5 Term/ComputationalMath/Labs/lab1/main.py
+++ 5 Term/ComputationalMath/Labs/lab1/main.py
@@ -9,25 +9,38 @@ import BasePart as BasePart
 '''
 Start data
 '''
+
 x_nodes = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
 y_nodes = np.array([3.37, 3.95, 3.73, 3.59, 3.15, 3.15, 3.05, 3.86, 3.60, 3.70, 3.02])
+
 
 left_border = 0
 right_border = 1
 
-сubicCoeff = BasePart.сubic_spline_coeff(x_nodes, y_nodes)
+'''
+Calc for graphs
+
+'''
+cubicCoeffs = BasePart.сubic_spline_coeff(x_nodes, y_nodes)
 x = np.linspace(left_border, right_border, 1000)
-S_x=[BasePart.сubic_spline(x, сubicCoeff) for x_i in x]
+
+S_x = [BasePart.сubic_spline(x_i, cubicCoeffs,x_nodes) for x_i in x]
 
 '''
-Рассчёт данных
+Output Graphs
 '''
-qs_coeff = сubic_spline_coeff(x_nodes, y_nodes)
-x = np.linspace(left_board, right_board, 100)
-S_x = [qubic_spline(x_i, qs_coeff, years, gdp_rus) for x_i in x]
 
-'''
-Вывод данных
+fig, ax = plt.subplots()
+ax.plot(x_nodes,y_nodes,label="Start date")
+ax.plot(x, S_x, label="Cubic spline interpolation")
+
+ax.legend(loc='upper left')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Base part')
+
+plt.show()
+
 '''
 fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(20, 20), dpi=100)
 axes.plot(x, S_x, color="purple", linewidth=1.5, linestyle="-", label="S(x)")
@@ -39,3 +52,4 @@ axes.grid()
 fig.tight_layout()
 plt.show()
 fig.savefig("сubic_spline_graphics.png")
+'''
