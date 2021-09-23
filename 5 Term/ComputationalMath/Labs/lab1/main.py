8883fbe96f2c0092e252eaab55fd4e5e1e45d754
Kekanium <stas280501@yandex.ru>
2021-09-23 15:50:25 +0300

    ComputationalMath lab1 start doing advanced part

--- 5 Term/ComputationalMath/Labs/lab1/main.py
+++ 5 Term/ComputationalMath/Labs/lab1/main.py
@@ -11,7 +11,7 @@ import AdvancedPart as AdvancedPart
 Start data
 '''
 
-x_nodes = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+x_nodes = np.array([0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.])
 y_nodes = np.array([3.37, 3.95, 3.73, 3.59, 3.15, 3.15, 3.05, 3.86, 3.60, 3.70, 3.02])
 
 left_border = 0
@@ -26,16 +26,47 @@ x = np.linspace(left_border, right_border, 1000)
 
 S_x = [BasePart.сubic_spline(x_i, cubicCoeffs, x_nodes) for x_i in x]
 
+
+x_tolerance = []
+for k in range(0, 1000):
+    x_tolerance.append(AdvancedPart.tolerance_Z(x_nodes))
+
+y_tolerance = []
+for k in range(0, 1000):
+    y_tolerance.append(AdvancedPart.tolerance_Z(x_nodes))
+
+
 '''
-Output Graphs
+Draw Graphs
 '''
 
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 20), dpi=300)
-axes[0].set_xticks(np.arange(0, 1.1, 0.1))
-axes[0].set_yticks(np.arange(3, 4.1, 0.1))
-axes[0].plot(x_nodes, y_nodes, color="blue", linewidth=1.5, linestyle="-", label="f(x)")  # Source data
-axes[0].plot(x_nodes, y_nodes, 'o', color="blue", linewidth=1.5)  # Source data
+fig, axes = plt.subplots(nrows=5, ncols=1, figsize=(20, 50), dpi=150)
+
+# Draw source graph for all plots and set grid,legend
+for i in range(5):
+    axes[i].set_xticks(np.arange(0, 1.1, 0.1))
+    axes[i].set_yticks(np.arange(3, 4.1, 0.1))
+    axes[i].plot(x_nodes, y_nodes, color="blue", linewidth=1.5, linestyle="-", label="f(x)")  # Source data
+    axes[i].plot(x_nodes, y_nodes, 'o', color="blue", linewidth=1.5)  # Source data
+    axes[i].legend(loc='upper right')
+    axes[i].grid()
+
+# Base part graph
+
 axes[0].plot(x, S_x, color="purple", linewidth=1.5, linestyle="-", label="S(x)")
-axes[0].legend(loc='upper right')
-axes[0].grid()
+
+# Advanced part graph
+
+# X tolerance polynomial
+axes[1].plot(x, AdvancedPart.Newton_poly(x, x_nodes, y_nodes), color="purple", linewidth=1.5, linestyle="-", label="S(x)")
+
+# y tolerance polynomial
+
+axes[2].plot(x, AdvancedPart.L(x, x_nodes, y_nodes), color="purple", linewidth=1.5, linestyle="-", label="S(x)")
+# X tolerance cubic
+
+
+# y tolerance cubic
+
+# Draw all plots
 plt.show()
