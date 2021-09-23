dd4208a8df010bc081045ea180040268c960d587
Stanislav_Notebook <stas280501@yandex.ru>
2021-09-23 18:37:11 +0300

    ComputationalMath lab1 start doing first graph iteration Lagrange

--- 5 Term/ComputationalMath/Labs/lab1/AdvancedPart.py
+++ 5 Term/ComputationalMath/Labs/lab1/AdvancedPart.py
@@ -20,5 +20,24 @@ def L(x, x_nodes, y_nodes):
     return result
 
 
-def tolerance_Z(input):
-    return input + np.random.normal(0, 0.01, len(input))
+def tolerance_Z(input_nodes):
+    return input_nodes + np.random.normal(0, 0.01, len(input_nodes))
+
+
+def confidence_spline(input_splines, percent):
+    result = []
+    h_u = []
+    h_m = []
+    h_l = []
+
+    len_input = len(input_splines)
+
+    h_l_position = int((len_input - (len_input * percent)) / 2) - 1
+    h_u_position = len_input - h_l_position - 2
+
+    input_splines = np.sort(input_splines, axis=0)
+    
+    h_u = input_splines[h_u_position]
+    h_l = input_splines[h_l_position]
+    h_m = np.mean(input_splines, axis=0)
+    return np.array((h_u, h_m, h_l))
--- 5 Term/ComputationalMath/Labs/lab1/main.py
+++ 5 Term/ComputationalMath/Labs/lab1/main.py
@@ -22,47 +22,56 @@ Calc for graphs
 
 '''
 cubicCoeffs = BasePart.сubic_spline_coeff(x_nodes, y_nodes)
+# TODO: change 100 to 1000
 x = np.linspace(left_border, right_border, 1000)
 
 S_x = [BasePart.сubic_spline(x_i, cubicCoeffs, x_nodes) for x_i in x]
 
-
+number_of_splines = 1000
 x_tolerance = []
-for k in range(0, 1000):
+for k in range(0, number_of_splines):
     x_tolerance.append(AdvancedPart.tolerance_Z(x_nodes))
 
 y_tolerance = []
-for k in range(0, 1000):
+for k in range(0, number_of_splines):
     y_tolerance.append(AdvancedPart.tolerance_Z(x_nodes))
-
+# Lagrange X
+lagrange_x_tolerance = []
+for i in x_tolerance:
+    lagrange_x_tolerance.append(AdvancedPart.L(x, i, y_nodes))
+lagrange_x_tolerance = AdvancedPart.confidence_spline(lagrange_x_tolerance, 0.9)
 
 '''
 Draw Graphs
 '''
 
-fig, axes = plt.subplots(nrows=5, ncols=1, figsize=(20, 50), dpi=150)
-
-# Draw source graph for all plots and set grid,legend
-for i in range(5):
-    axes[i].set_xticks(np.arange(0, 1.1, 0.1))
-    axes[i].set_yticks(np.arange(3, 4.1, 0.1))
-    axes[i].plot(x_nodes, y_nodes, color="blue", linewidth=1.5, linestyle="-", label="f(x)")  # Source data
-    axes[i].plot(x_nodes, y_nodes, 'o', color="blue", linewidth=1.5)  # Source data
-    axes[i].legend(loc='upper right')
-    axes[i].grid()
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(20, 50), dpi=150)
 
 # Base part graph
+axes[0].set_xticks(np.arange(0, 1.1, 0.1))
+axes[0].set_yticks(np.arange(3, 4.1, 0.1))
+axes[0].plot(x_nodes, y_nodes, color="blue", linewidth=1.5, linestyle="-", label="f(x)")  # Source data
+axes[0].plot(x_nodes, y_nodes, 'o', color="blue", linewidth=1.5)  # Source data
+axes[0].legend(loc='best')
+axes[0].grid()
 
 axes[0].plot(x, S_x, color="purple", linewidth=1.5, linestyle="-", label="S(x)")
 
 # Advanced part graph
 
 # X tolerance polynomial
-axes[1].plot(x, AdvancedPart.Newton_poly(x, x_nodes, y_nodes), color="purple", linewidth=1.5, linestyle="-", label="S(x)")
+axes[1].plot(x_nodes, y_nodes, color="blue", linewidth=1.5, linestyle="-", label="f(x)")  # Source data
+axes[1].plot(x_nodes, y_nodes, 'o', color="blue", linewidth=1.5)  # Source data
+axes[0].legend(loc='best')
+axes[0].grid()
 
+axes[1].plot(x, lagrange_x_tolerance[0], color="red", linewidth=1.5, linestyle="-", label="h_u")
+axes[1].plot(x, lagrange_x_tolerance[2], color="orange", linewidth=1.5, linestyle="-", label="h_l")
+axes[1].plot(x, lagrange_x_tolerance[1], color="violet", linewidth=1.5, linestyle="--", label="h_m")
+axes[1].fill_between(x, lagrange_x_tolerance[0], lagrange_x_tolerance[2],alpha=0.1, color="blue")
 # y tolerance polynomial
 
-axes[2].plot(x, AdvancedPart.L(x, x_nodes, y_nodes), color="purple", linewidth=1.5, linestyle="-", label="S(x)")
+
 # X tolerance cubic
 
 
