3d7a489d0ed584a2ae4fe07c129f274aed2f2c25
Kekanium <stas280501@yandex.ru>
2021-11-07 01:33:03 +0300

    5 Term Data Bases coursework done 60%

--- 5 Term/Data Bases coursework/src/edit/routes.py
+++ 5 Term/Data Bases coursework/src/edit/routes.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 from flask_table import Table, Col
 from flask_table.html import element
 
@@ -9,8 +9,6 @@ from sql.SQLmaster import SQLmaster
 
 edit_app = Blueprint('edit', __name__, template_folder='templates')
 SQLserver = SQLmaster(json.load(open('config/db_config.json', 'r')))
-
-
 
 
 class ExternalURLCol(Col):
@@ -29,7 +27,8 @@ class ItemTableEditUsers(Table):
     Login = Col('Логин')
     PasswordL = Col('Пароль')
     AccessLevel = Col('Уровень доступа')
-    Link = ExternalURLCol(' ', url_attr='url', attr='name')
+    UpdateLink = ExternalURLCol(' ', url_attr='urlUpdate', attr='nameUpdate')
+    DeleteLink = ExternalURLCol(' ', url_attr='urlDelete', attr='nameDelete')
 
 
 # region General
@@ -47,34 +46,57 @@ def edit_index():
 def edit_users():
     result = SQLserver.request('edit_AllUsersInfo.sql')
     for user in result:
-        test = url_for('edit.edit_users')
         idLogin = user['idLogin']
-        user['url'] = test + f'/{idLogin}'
-        user['name'] = f'Редактировать'
+        user['urlUpdate'] = url_for('edit.edit_users') + f'/{idLogin}'
+        user['nameUpdate'] = f'Редактировать'
+        user['urlDelete'] = url_for('edit.edit_users') + f'/delete' + f'/{idLogin}'
+        user['nameDelete'] = f'Удалить'
         print()
     table = ItemTableEditUsers(result)
 
-
-
     HTMLtable = table.__html__()
-    file = open(r'.\requests\templates\edit_users_child.html', 'w', encoding='utf-8')
-
+    file = open(r'.\edit\templates\edit_users_child.html', 'w', encoding='utf-8')
     file.write('{% extends \'edit_users_base.html\' %}{% block child %}<div class="article_3">')
-    if HTMLtable == "<p>No Items</p>":
-        file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
-    else:
-        file.write(HTMLtable)
+    file.write(HTMLtable)
     file.write('</div>{% endblock %}')
     file.close()
 
-    return render_template('edit_users_child.html')  # TODO делать красивую страницу
+    return render_template('edit_users_child.html')
 
 
-@edit_app.route('/users/<user>')
+@edit_app.route('/users/<user>', methods=['GET', 'POST'])
 @group_permission_decorator
 def edit_user(user):
-    result = SQLserver.request('edit_getUser.sql', id=user)
+    if request.method == 'GET':
+        result = SQLserver.request('edit_getUser.sql', id=user)[0]
+        return render_template('edit_user.html', idLogin=result['idLogin'], Login=result['Login'],
+                               PasswordL=result['PasswordL'], AccessLevel=result['AccessLevel'])
+    else:
+        idLogin = int(user)
+        Login = request.form.get('Login')
+        PasswordL = request.form.get('PasswordL')
+        AccessLevel = int(request.form.get('AccessLevel'))
+        SQLserver.update_insert('edit_updateUser.sql', idLogin=idLogin, Login=Login, PasswordL=PasswordL,
+                                AccessLevel=AccessLevel)
+        return render_template('edit_result.html', url_back='../users')
 
+@edit_app.route('/users/add', methods=['GET', 'POST'])
+@group_permission_decorator
+def edit_insert_user():
+    if request.method == 'GET':
+        return render_template('edit_insert_user.html')
+    else:
+        Login = request.form.get('Login')
+        PasswordL = request.form.get('PasswordL')
+        AccessLevel = int(request.form.get('AccessLevel'))
+        SQLserver.update_insert('edit_insertUser.sql', Login=Login, PasswordL=PasswordL,
+                                AccessLevel=AccessLevel)
+        return render_template('edit_result.html', url_back='../users')
 
-    return f'{result}'
+@edit_app.route('/users/delete/<user>')
+@group_permission_decorator
+def edit_delete_user(user):
+    SQLserver.update_insert('edit_deleteUser.sql', idLogin=int(user))
+    return render_template('edit_delete_user.html', idLogin=int(user))
+
 # endregion
--- 5 Term/Data Bases coursework/src/sql/SQLmaster.py
+++ 5 Term/Data Bases coursework/src/sql/SQLmaster.py
@@ -19,3 +19,13 @@ class SQLmaster:
             cursorDict.execute(request)
             resulting = cursorDict.fetchall()
             return resulting
+
+    def update_insert(self, filename, **kwargs):
+        cursor = self.db_connect.cursor
+        request = self.db_request.get(filename, **kwargs)
+        if cursor is None:
+            raise ValueError('Cursor is None')
+        elif cursor:
+            cursor.execute(request)
+            self.db_connect.conn.commit()
+            return None
